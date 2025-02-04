65a409361d91e646311131d3e049e84a89c1689a
Nedim Kajanija <kajanija15@gmail.com>
2025-02-04 07:36:49 +0100

    Sidebar update

--- src/components/SidebarMenuItem.jsx
+++ src/components/SidebarMenuItem.jsx
@@ -5,7 +5,7 @@ import { Link, NavLink } from 'react-router-dom'
 
 function SidebarMenuItem({icon,title,link}) {
   return (
-    <NavLink to={link} end className={({ isActive }) => `${isActive ? 'opacity-100' : 'opacity-50'} text-center cursor-pointer hover:opacity-75 p-2 transition-all duration-300 ease-in-out`}>
+    <NavLink to={link} className={({ isActive }) => `${isActive ? 'opacity-100' : 'opacity-50'} text-center cursor-pointer hover:opacity-75 p-2 transition-all duration-300 ease-in-out`}>
             <FontAwesomeIcon icon={icon} className='text-theme-white mb-3' />
             <p className='text-theme-white'>{title}</p>
     </NavLink>
